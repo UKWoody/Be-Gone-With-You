cfa911e7634b571da8653ca8a9df78a7ba56b7d0
UKWoody <69971161+UKWoody@users.noreply.github.com>
2026-02-14 11:57:54 +0000

    Update config.py to use placeholders for sensitive data
    Replaced sensitive token and guild ID with placeholders.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "MTQ3MTk2MTUwNDg2NDgwMDk4MA.GmcTQB._LPD_N_XMoWZGkmiGlpcs-t6XuTHhdaoqg-1dE"
-GUILD_ID = 1471984401645572379  # replace with your server ID
+TOKEN = "YOUR_TOKEN"
+GUILD_ID = SERVER_ID  # replace with your server ID
 BOT_VERSION = "1.0.0"
 SETTINGS_FILE = "data/settings.json"
